7d3c4dc4a85fab9a2377052373d3b26801464def
molten-banana <molten.banana@protonmail.com>
2026-05-26 15:55:38 +0200

    Fixing mediamanager function

--- admin/admin.js
+++ admin/admin.js
@@ -194,9 +194,9 @@ const sectionsTab =
     'sections-tab'
   );
 
-const contentList =
+const contentPanel =
   document.getElementById(
-    'content-list'
+    'content-panel'
   );
 
 const cardsPanel =
